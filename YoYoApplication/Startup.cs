d6928447f300b02b14ba851b4cee5204ac4717cc
Ayush Joshi <rockingayush1995@gmail.com>
2020-10-28 13:46:31 +0530

    image added with some minor changes

--- YoYoApplication/Startup.cs
+++ YoYoApplication/Startup.cs
@@ -28,7 +28,6 @@ namespace YoYoApplication
             services.AddSingleton<IShuttleRepository, ShuttleRepository>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
